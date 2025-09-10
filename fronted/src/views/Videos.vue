2f755bc963fdf58622db0a9a07118261af7cdd68
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-09-10 10:49:00 -0600

    Se termino la seccion de videos

--- fronted/src/views/Videos.vue
+++ fronted/src/views/Videos.vue
@@ -1,41 +1,32 @@
 <script setup>
-// Importaciones
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
 import Header from '@/components/Header.vue'
 import Fotter from '@/components/Fotter.vue'
+import Fondo from '@/components/Fondo.vue';
 
 const API_URL = import.meta.env.VITE_API_URL;
-
 const videos = ref([]);
 
 const getVideos = async () => {
-    try {
-        // Usa `axios.get` con la URL construida de forma similar a tu ejemplo
-        const response = await axios.get(`${API_URL}videos`);
-        
-        // Asigna el array de datos dentro de la clave 'data' de la respuesta
-        videos.value = response.data.data;
-        
-        // Opcional: Para verificar que se reciben los datos
-        console.log("Datos de videos recibidos:", videos.value);
-    } catch (error) {
-        console.error("Error al obtener los videos:", error);
-    }
+  try {
+    const response = await axios.get(`${API_URL}videos`);
+    videos.value = response.data?.data || [];
+    console.log("Datos de videos recibidos:", videos.value);
+  } catch (error) {
+    console.error("Error al obtener los videos:", error);
+  }
 };
 
-onMounted(() => {
-    getVideos();
-});
+onMounted(getVideos);
 </script>
 
 <template>
   <Header></Header>
-
-  <div class="fondo_animado_videos"></div>
+  <Fondo></Fondo>
 
   <div class="contenido_videos">
-    <div class="container contenedor_videos my-3">
+    
       <div class="row">
         <div class="col d-flex justify-content-center">
           <h4>Videos</h4>
@@ -43,8 +34,8 @@ onMounted(() => {
         <hr class="estilo_hr" />
       </div>
 
-      <div v-for="video in videos" :key="video.id">
-        <div class="container contenedor_videos my-3">
+      <div v-if="videos.length > 0">
+        <div v-for="video in videos" :key="video.id" class="container contenedor_videos my-3">
           <div class="row">
             <div class="col d-flex justify-content-center">
               <h5 class="title_video"><strong>{{ video.nombre }}</strong></h5>
@@ -57,21 +48,22 @@ onMounted(() => {
           </div>
           <div class="row">
             <div class="col d-flex justify-content-center">
+              <!-- Si es YouTube -->
               <iframe
-                v-if="video.id_youtube"
+                v-if="video.video_url.includes('youtube.com')"
                 class="config_videos"
                 width="560"
                 height="315"
                 :src="video.video_url"
                 title="YouTube video player"
-                frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerpolicy="strict-origin-when-cross-origin"
                 allowfullscreen
               ></iframe>
 
+              <!-- Si es video local -->
               <video
-                v-else-if="video.video"
+                v-else
                 :src="video.video_url"
                 class="config_videos"
                 width="560"
@@ -83,13 +75,14 @@ onMounted(() => {
         </div>
       </div>
 
-      <div v-if="!videos || videos.length === 0" class="col">
+      <div v-else class="col">
         <p>No se encontraron videos para mostrar.</p>
       </div>
-    </div>
+    
   </div>
 
-  <Fotter />
+  <Fotter></Fotter>
 </template>
+
 
 
